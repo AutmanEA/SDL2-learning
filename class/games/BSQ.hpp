b33afc7b664f3634ecad54ec828cc7eb14b5527a
Autman El Atmioui <ael-atmi@1C7.42angouleme.fr>
2025-09-01 14:48:53 +0200

    updating bsq, added drawing rectangle (not perfect)

--- class/games/BSQ.hpp
+++ class/games/BSQ.hpp
@@ -5,6 +5,7 @@
 #include <ctime>
 #include <Screen.hpp>
 
+# define EMPTY		0
 # define USER_FILL	1
 # define OBSTACLE	2
 # define SOLUTION	3
@@ -29,25 +30,37 @@ class BSQ
 			NEW,
 			VALIDATE,
 			RESOLVE,
-			ERASE
+			CLEAR
 		}	BSQ_Actions;
 
 		int		_board[50][50];
 		int		_boardBuffer[50][50];
+		int		_heatmap[50][50];
 		int		_boardWidth;
 		int		_boardHeight;
 		int		_cellSize;
 
+		//! ATTENTION, LA RESOLUTION DU BSQ NE MARCHE PAS, J'AI INVERSE X et Y!!!
+
 		// bool	_validate;
-		// bool	_draw;
+		bool	_draw;
 		// bool	_erase;
 
 		std::map<SDL_Keycode, BSQ_Actions>	_keymap;
 		std::map<BSQ_Actions, SDL_Rect>		_buttons;
 
+		int		_squareStartX;
+		int		_squareStartY;
+		int		_squareCurrentX;
+		int		_squareCurrentY;
+
 		void	_eraseBoard(void);
+		void	_clearBoard(void);
 		void	_copyBoard(void);
 		void	_randomBoard(int probability);
+		void	_newBoard(void);
+		void	_initSolution(void);
+		void	_initHeatmap(void);
 
 		void	_eventsOnMainScreen(SDL_Event &event, Screen *mainScreen);
 		void	_eventsOnToolboxScreen(SDL_Event &event, Screen *toolboxScreen);
--- src/games/BSQ.cpp
+++ src/games/BSQ.cpp
@@ -3,27 +3,31 @@
 BSQ::BSQ() :
 	_board(),
 	_boardBuffer(),
+	_heatmap(),
 	_boardWidth(50),
 	_boardHeight(50),
-	_cellSize(12)
+	_cellSize(12),
 	// _validate(false),
-	// _draw(false),
+	_draw(false),
 	// _erase(false)
+	_squareStartX(0),
+	_squareStartY(0),
+	_squareCurrentX(0),
+	_squareCurrentY(0)
 {
 	_keymap[SDLK_SPACE] = NEW;
 	_keymap[SDLK_RETURN] = VALIDATE;
 	_keymap[SDLK_r] = RESOLVE;
-	_keymap[SDLK_BACKSPACE] = ERASE;
+	_keymap[SDLK_BACKSPACE] = CLEAR;
 
 	//TODO: class Button pour dessiner les boutons, avec position et taille, et pourquoi pas type du bouton.
 	_buttons[VALIDATE] = {20, 20, 50, 50};
 	_buttons[NEW] = {20, 90, 50, 50};
 	_buttons[RESOLVE] = {20, 160, 50, 50};
-	_buttons[ERASE] = {20, 230, 50, 50};
+	_buttons[CLEAR] = {20, 230, 50, 50};
 
-	std::srand(static_cast<unsigned>(std::time(NULL)));
-	_randomBoard(std::rand() % 20 + 5);
-	_copyBoard();
+	_newBoard();
+	_clearBoard();
 }
 
 BSQ::~BSQ() {}
@@ -33,13 +37,13 @@ void	BSQ::run()
 
 }
 
-void	BSQ::_copyBoard(void)
+void	BSQ::_newBoard(void)
 {
-	for (int i = 0; i < _boardWidth; i++)
-	{
-		for (int j = 0; j < _boardHeight; j++)
-			_boardBuffer[j][i] = _board[j][i];
-	}
+	_eraseBoard();
+	std::srand(static_cast<unsigned>(std::time(NULL)));
+	_randomBoard(std::rand() % 15 + 5);
+	_initSolution();
+	_copyBoard();
 }
 
 void	BSQ::_eraseBoard(void)
@@ -48,6 +52,15 @@ void	BSQ::_eraseBoard(void)
 	{
 		for (int j = 0; j < _boardHeight; j++)
 			_board[j][i] = 0;
+	}
+}
+
+void	BSQ::_clearBoard(void)
+{
+	for (int i = 0; i < _boardWidth; i++)
+	{
+		for (int j = 0; j < _boardHeight; j++)
+			_board[j][i] = _boardBuffer[j][i];
 	}
 }
 
@@ -65,6 +78,72 @@ void	BSQ::_randomBoard(int probability)
 	}
 }
 
+void	BSQ::_initSolution(void)
+{
+	int	save_i = 0;
+	int	save_j = 0;
+	int	saveMax = 0;
+
+	_initHeatmap();
+
+	for (int i = 1; i < _boardWidth; i++)
+	{
+		for (int j = 1; j < _boardHeight; j++)
+		{
+			if (_board[j][i] == OBSTACLE)
+				_heatmap[j][i] = 0;
+			else
+				_heatmap[j][i] = std::min(std::min(_heatmap[j][i - 1], _heatmap[j - 1][i]), _heatmap[j - 1][i - 1]) + 1;
+			if (_heatmap[j][i] > saveMax)
+			{
+				saveMax = _heatmap[j][i];
+				save_i = i;
+				save_j = j;
+			}
+		}
+	}
+
+	//! faut que je change pour que ca trouve toutes les solutions, pas juste la premiere dispo
+
+	for (int i = 0; i < saveMax; i++)
+	{
+		int	tmp = save_j;
+		for (int j = 0; j < saveMax; j++)
+		{
+			_board[tmp][save_i] = SOLUTION;
+			tmp--;
+		}
+		save_i--;
+	}
+}
+
+void	BSQ::_initHeatmap(void)
+{
+	for (int i = 0; i < _boardWidth; i++)
+	{
+		if (_board[0][i] != OBSTACLE)
+			_heatmap[0][i] = 1;
+		else
+			_heatmap[0][i] = 0;
+	}
+	for (int j = 0; j < _boardHeight; j++)
+	{
+		if (_board[j][0] != OBSTACLE)
+			_heatmap[j][0] = 1;
+		else
+			_heatmap[j][0] = 0;
+	}
+}
+
+void	BSQ::_copyBoard(void)
+{
+	for (int i = 0; i < _boardWidth; i++)
+	{
+		for (int j = 0; j < _boardHeight; j++)
+			_boardBuffer[j][i] = _board[j][i];
+	}
+}
+
 void	BSQ::render(Screen **mainScreen, Screen **toolboxScreen)
 {
 	if (*toolboxScreen)
@@ -74,7 +153,8 @@ void	BSQ::render(Screen **mainScreen, Screen **toolboxScreen)
 			switch (b->first) {
 				case NEW:   SDL_SetRenderDrawColor((*toolboxScreen)->getRenderer(), 0, 200, 0, 255); break;
 				case VALIDATE:  SDL_SetRenderDrawColor((*toolboxScreen)->getRenderer(), 0, 0, 200, 255); break;
-				case ERASE:  SDL_SetRenderDrawColor((*toolboxScreen)->getRenderer(), 200, 200, 0, 255); break;
+				case RESOLVE:  SDL_SetRenderDrawColor((*toolboxScreen)->getRenderer(), 0, 200, 200, 255); break;
+				case CLEAR:  SDL_SetRenderDrawColor((*toolboxScreen)->getRenderer(), 200, 200, 0, 255); break;
 				default: break;
 			}
 			SDL_RenderFillRect((*toolboxScreen)->getRenderer(), &b->second);
@@ -95,17 +175,39 @@ void	BSQ::render(Screen **mainScreen, Screen **toolboxScreen)
 			cell.x = x * _cellSize + (WIDTH - (_boardWidth * _cellSize)) / 2;
 			cell.y = y * _cellSize + (HEIGHT - (_boardHeight * _cellSize)) / 2;
 
+			if (_draw)
+			{
+				int x1 = std::min(_squareStartX, _squareCurrentX);
+				int y1 = std::min(_squareStartY, _squareCurrentY);
+				int x2 = std::max(_squareStartX, _squareCurrentX);
+				int y2 = std::max(_squareStartY, _squareCurrentY);
+
+				SDL_Rect rect = {
+					x1 * _cellSize + (WIDTH - (_boardWidth * _cellSize)) / 2,
+					y1 * _cellSize + (HEIGHT - (_boardHeight * _cellSize)) / 2,
+					(x2 - x1 + 1) * _cellSize,
+					(y2 - y1 + 1) * _cellSize
+				};
+
+				SDL_SetRenderDrawColor((*mainScreen)->getRenderer(), 255, 0, 0, 255);
+				SDL_RenderFillRect((*mainScreen)->getRenderer(), &rect);
+			}
 			if (_board[y][x] == USER_FILL)
 			{
 				SDL_SetRenderDrawColor((*mainScreen)->getRenderer(), 255, 255, 255, 255);
 				SDL_RenderFillRect((*mainScreen)->getRenderer(), &cell);
 			}
-			else if (_board[y][x] == OBSTACLE)
+			if (_board[y][x] == OBSTACLE)
 			{
 				SDL_SetRenderDrawColor((*mainScreen)->getRenderer(), 120, 120, 120, 255);
 				SDL_RenderFillRect((*mainScreen)->getRenderer(), &cell);
 			}
-			else
+			if (_board[y][x] == SOLUTION) //!a delete >
+			{
+				SDL_SetRenderDrawColor((*mainScreen)->getRenderer(), 0, 255, 0, 255);
+				SDL_RenderFillRect((*mainScreen)->getRenderer(), &cell);
+			}//! <
+			if (_board[y][x] == EMPTY)
 			{
 				SDL_SetRenderDrawColor((*mainScreen)->getRenderer(), 15, 5, 5, 20);
 				SDL_RenderDrawRect((*mainScreen)->getRenderer(), &cell);
@@ -134,61 +236,78 @@ void	BSQ::render(Screen **mainScreen, Screen **toolboxScreen)
 
 void	BSQ::_eventsOnMainScreen(SDL_Event &event, Screen *mainScreen)
 {
-	(void)mainScreen;
-	
+	static bool	ignoreFirstMouseUp = true;
+
 	if (event.type == SDL_KEYDOWN)
 	{
 		std::map<SDL_Keycode, BSQ_Actions>::iterator it = _keymap.find(event.key.keysym.sym);
 		if (it != _keymap.end()) {
 			switch (it->second) {
 				case NEW:
-					_eraseBoard();
-					std::srand(static_cast<unsigned>(std::time(NULL)));
-					_randomBoard(std::rand() % 20 + 5);
-					_copyBoard();
+					_newBoard();
 					break;
 				case VALIDATE:
 					// _resolveOneStep();
 					break;
-				case ERASE:
-					_eraseBoard();
+				case CLEAR:
+					_clearBoard();
 					break;
 				default:
 					break;
 			}
 		}
 	}
-	// else if ((event.type == SDL_MOUSEBUTTONDOWN || event.type == SDL_MOUSEBUTTONUP || event.type == SDL_MOUSEMOTION) && event.button.windowID == mainScreen->getID())
-	// {
-	// 	if (event.type == SDL_MOUSEBUTTONDOWN)
-	// 	{
-	// 		if (event.button.button == SDL_BUTTON_LEFT)
-	// 			_draw = true;
+	else if ( (event.type == SDL_MOUSEBUTTONDOWN || event.type == SDL_MOUSEBUTTONUP || event.type == SDL_MOUSEMOTION)
+				&& event.button.windowID == mainScreen->getID() )
+	{
+		if (event.type == SDL_MOUSEBUTTONDOWN)
+		{
+			if (event.button.button == SDL_BUTTON_LEFT)
+			{
+				_squareStartX = (event.button.x - ((WIDTH - (_boardWidth * _cellSize)) / 2)) / _cellSize;
+				_squareStartY = (event.button.y - ((HEIGHT - (_boardHeight * _cellSize)) / 2)) / _cellSize;
+				_draw = true;
+			}
 	// 		else if (event.button.button == SDL_BUTTON_RIGHT)
 	// 			_erase = true;
-	// 	}
-	// 	else if (event.type == SDL_MOUSEBUTTONUP)
-	// 	{
-	// 		if (event.button.button == SDL_BUTTON_LEFT)
-	// 			_draw = false;
+		}
+		else if (event.type == SDL_MOUSEBUTTONUP)
+		{
+			_clearBoard();
+			_draw = false;
+
+			if (ignoreFirstMouseUp == true)
+				ignoreFirstMouseUp = false;
+			else if (event.button.button == SDL_BUTTON_LEFT)
+			{
+				int	x1 = std::min(_squareStartX, _squareCurrentX);
+				int	y1 = std::min(_squareStartY, _squareCurrentY);
+				int	x2 = std::max(_squareStartX, _squareCurrentX);
+				int	y2 = std::max(_squareStartY, _squareCurrentY);
+
+				for (int y = y1; y <= y2; ++y)
+				{
+					for (int x = x1; x <= x2; ++x)
+					{
+						if (x >= 0 && x < _boardWidth && y >= 0 && y < _boardHeight)
+							_board[y][x] = USER_FILL;
+					}
+				}
+
+			}
 	// 		else if (event.button.button == SDL_BUTTON_RIGHT)
 	// 			_erase = false;
-	// 	}
-	// 	else if (event.type == SDL_MOUSEMOTION)
-	// 	{
-	// 		int mouseX = (event.button.x - ((WIDTH - (_boardWidth * _cellSize)) / 2)) / _cellSize;
-	// 		int mouseY = (event.button.y - ((HEIGHT - (_boardHeight * _cellSize)) / 2)) / _cellSize;
-
-	// 		if (_draw)
-	// 		{
-	// 			_board[mouseY][mouseX] = 1;
-	// 		}
+		}
+		else if (event.type == SDL_MOUSEMOTION && _draw)
+		{
+			_squareCurrentX = (event.motion.x - ((WIDTH - (_boardWidth * _cellSize)) / 2)) / _cellSize;
+			_squareCurrentY = (event.motion.y - ((HEIGHT - (_boardHeight * _cellSize)) / 2)) / _cellSize;
 	// 		if (_erase)
 	// 		{
-	// 			_board[mouseY][mouseX] = 0;
+	// 			_board[_squareCurrentY][_squareCurrentX] = 0;
 	// 		}
-	// 	}
-	// }
+		}
+	}
 }
 
 void	BSQ::_eventsOnToolboxScreen(SDL_Event &event, Screen *toolboxScreen)
@@ -205,13 +324,10 @@ void	BSQ::_eventsOnToolboxScreen(SDL_Event &event, Screen *toolboxScreen)
 				switch (b->first)
 				{
 					case NEW:
-						_eraseBoard();
-						std::srand(static_cast<unsigned>(std::time(NULL)));
-						_randomBoard(std::rand() % 20 + 5);
-						_copyBoard();
+						_newBoard();
 						break;
-					case ERASE:
-						_eraseBoard();
+					case CLEAR:
+						_clearBoard();
 						break;
 					default:
 						break;
